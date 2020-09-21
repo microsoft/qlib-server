a467702d7e1955ecc3428666154522c34143f3f1
zhupr <zhu.pengrong@foxmail.com>
2020-09-22 00:08:58 +0800

    Modify version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ REQUIRED = [
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = "\n" + f.read()
 
 # Where the magic happens:
